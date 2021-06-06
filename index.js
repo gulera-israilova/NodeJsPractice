a9ee04e5e04ee6de343a8fc6a02af8060ff6eee7
Gulera Israilova <guleraisrailova@gmail.com>
2021-06-06 15:33:08 +0600

    Complete task

--- index.js
+++ index.js
@@ -1,28 +1,45 @@
 const fs = require('fs');
 
-fs.readFile('input.txt', function(err, data) {
-    if(err) throw err;
-    const array1 = data.toString().split("\n");
+let array1 = fs.readFileSync('input.txt').toString().split("\n"),
+    array2 = fs.readFileSync('patterns.txt').toString().split("\n");
 
-    fs.readFile('patterns.txt', function(err, data) {
-        if(err) throw err;
-        const array2 = data.toString().split("\n");
+let mode1Result = [],
+    mode2Result = [],
+    mode3Result = [];
 
-        for(i in array1) {
-            for(j in array2){
-                if (array1[i]===array2[j]){
-                    console.log(array1[i]);
-                }}}
-        for(i in array1) {
-            for(j in array2){
-                if (array1[i]===array2[j] && j===i){
-                    console.log(array1[i]);
-                }
-            }}
+array1.forEach((array1Item, array1Index) => {
+    array2.forEach((array2Item, array2Index) => {
+        if (array1Item === array2Item) {
+            mode1Result.push(array1Item);
+        }
+
+        if (array1Item === array2Item && array1Index === array2Index) {
+            mode2Result.push(array1Item)
+        }
+
+        if (diff(array1Item, array2Item) === 1) {
+            mode3Result.push(array1Item);
+        }
+    })
+})
+
+function diff(str1, str2) {
+    let diff = 0;
+    if (str1.length !== str2.length) {
+        return
+    }
+    str2.split('').forEach(function (item, index) {
+        if (item !== str1.charAt(index)) {
+            diff++;
+        }
     });
-});
 
+    return diff;
+}
 
-
+console.log('Source files data: ', array1, ',', array2)
+console.log('Mode 1 result: ', mode1Result)
+console.log('Mode 2 result: ', mode2Result)
+console.log('Mode 3 result: ', mode3Result)
 
 
